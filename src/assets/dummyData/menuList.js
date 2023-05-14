833b306137841a32f456c39da2b4fcf32a0540df
James <mirageapache@gmail.com>
2023-05-14 11:04:11 +0800

    Build menuList

---
+++ src/assets/dummyData/menuList.js
@@ -0,0 +1,6 @@
+export const menuList = [
+  {"name":"鮪魚蛋餅", "price":30, "img":"tuna-egg-cake.jpg"},
+  {"name":"起司蛋餅", "price":30, "img":"cheese-egg-cake.jpg"},
+  {"name":"嫩煎雞腿堡", "price":50, "img":"chicken-burger.jpg"},
+  // {"name":"","price":"","img":".jpg"},
+]
--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -1,10 +1,42 @@
 import 'styles/css/menuList.css'
+import { menuList } from 'assets/dummyData/menuList.js'
+import image from 'assets/images/chesee-egg-cake.jpg'
+import {ReactComponent as IconAdd } from 'assets/icons/add.svg'
+
+interface ItemData {
+  name: string;
+  price: number;
+  img: string;
+}
 
 export default function MenuList(){
+
+  const item = menuList.map((item: ItemData, index: number)=>{
+    return <MenuItem key={index} data={item} />
+  })
+
   return(
     <div className="menu-list">
-      <h3>MenuList</h3>
-
+      <div className='list-mode'>
+        {item}
+      </div>
     </div>
   )
 }
+
+function MenuItem( props:{ data: ItemData } ){
+  const {data} = props
+
+  return(
+    <div className='menu-item'>
+      <img className='item-img' src={image} alt="item images" />
+      <div className='item-info'>
+        <p className='item-name'>{data.name}</p>
+        <p className='item-price'>NT. {data.price}</p>
+        <IconAdd className='icon add-icon' />
+      </div>
+    </div>
+  )
+}
+
+
