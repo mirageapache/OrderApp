3076c4c35535da5729ac242a6c2878ec16ffd1b2
James <mirageapache@gmail.com>
2023-05-17 16:40:32 +0800

    Build item model

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -3,6 +3,8 @@ import { menuList } from 'assets/dummyData/menuList.js'
 import image from 'assets/images/chesee-egg-cake.jpg'
 import {ReactComponent as IconAdd } from 'assets/icons/add.svg'
 import { useSetting } from 'context/SettingContext';
+import ItemModel from './ItemModel';
+import { useState } from 'react';
 
 interface ItemData {
   name: string;
@@ -12,13 +14,15 @@ interface ItemData {
 
 export default function MenuList(){
   const { menuMode } = useSetting();
+  const [modelState, setModelState] = useState<boolean>(false)
 
   const item = menuList.map((item: ItemData, index: number)=>{
-    return <MenuItem key={index} data={item} />
+    return <MenuItem key={index} data={item} modelState={modelState} handleClick={() => setModelState(!modelState)} />
   })
 
   return(
     <div className="menu-list">
+      { modelState && <ItemModel handleClick={() => setModelState(!modelState)}/> }
       <div className={menuMode}>
         {item}
       </div>
@@ -26,8 +30,8 @@ export default function MenuList(){
   )
 }
 
-function MenuItem( props:{ data: ItemData } ){
-  const {data} = props
+function MenuItem( props:{ data: ItemData; modelState: boolean; handleClick: (value: boolean) => void } ){
+  const {data, modelState, handleClick} = props
 
   return(
     <div className='menu-item'>
@@ -35,7 +39,7 @@ function MenuItem( props:{ data: ItemData } ){
       <div className='item-info'>
         <p className='item-name'>{data.name}</p>
         <p className='item-price'>NT. {data.price}</p>
-        <IconAdd className='icon add-icon' />
+        <IconAdd className='icon add-icon' onClick={() => handleClick(!modelState)} />
       </div>
     </div>
   )
