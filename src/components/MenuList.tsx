6885df9e317d994023a7f0b327337112b79e4bba
James <mirageapache@gmail.com>
2023-05-24 15:35:21 +0800

    fetch MenuList API

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -1,10 +1,13 @@
 import 'styles/css/menuList.css'
-import { menuList } from 'assets/dummyData/menuList.js'
-import image from 'assets/images/chesee-egg-cake.jpg'
-import {ReactComponent as IconAdd } from 'assets/icons/add.svg'
+import axios from 'axios';
 import { useSetting } from 'context/SettingContext';
 import ItemModel from './ItemModel';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { apiURL } from 'api';
+
+// import { menuList } from 'assets/dummyData/menuList.js'
+import image from 'assets/images/chesee-egg-cake.jpg'
+import {ReactComponent as IconAdd } from 'assets/icons/add.svg'
 
 interface ItemData {
   name: string;
@@ -14,7 +17,18 @@ interface ItemData {
 
 export default function MenuList(){
   const { menuMode } = useSetting();
-  const [modelState, setModelState] = useState<boolean>(false)
+  const [modelState, setModelState] = useState<boolean>(false);
+  const [menuList, setMenuList] = useState<any>([]);
+
+  useEffect(() => {
+    if(menuList.length === 0){
+      axios.get(`${apiURL}/menulist`)
+      .then(res => {
+        setMenuList(res.data);
+      })
+      .catch(error => console.log(error))
+    }
+  },[menuList])
 
   const item = menuList.map((item: ItemData, index: number)=>{
     return <MenuItem key={index} data={item} modelState={modelState} handleClick={() => setModelState(!modelState)} />
